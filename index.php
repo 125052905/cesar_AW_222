bb3b0dbb23b9bc5b99af4c190630381356895d3e
125052905 <125052905@upq.edu.mx>
2026-09-17 17:18:12 -0600

    Trabajamos con php y con estructura de html

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- <?php 
+echo ("Hola mundo")
+?> --> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+     h1 {
+        color: purple;
+     }
+     div{ /*Para el fondo*/
+    background-color: #6200ff;
+     }
+    </style>
+</head>
+<body>
+    <h1>Titulo</h1>
+    <h2>Hola Crayola</h2>
+    <h3>Hola Crayola</h3>
+    <h4>Hola Crayola</h4>
+    <h5>Hola Crayola</h5>
+
+
+<div>
+    <h2> Titulo del parrafo </h2>
+    
+    <p> Este es un parrafo </p>
+    
+</div>
+
+</body>
+</html>
+
